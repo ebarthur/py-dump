eafed10308feafcfbb2932030c138b5661bfa66b
Aartt! <151869774+ebarthur@users.noreply.github.com>
2023-12-13 16:57:08 +0000

    Update code.py
    These comments provide explanations for each section of your code, making it easier to understand the functionality and flow of the program.

--- guess game/code.py
+++ guess game/code.py
@@ -1,46 +1,57 @@
+# Import the random module for generating random numbers
 import random
-rules = "The System will select a number between 0 to 10 and you have to guess what number it is. You have 3 chances"
 
+# Display the rules of the game
+rules = "The System will select a number between 0 to 10, and you have to guess what number it is. You have 3 chances"
 print(rules)
 
+# Define a function to repeat the game
 def repeat():
-    secret_number = random.randrange(0,10)
+    # Generate a random number between 0 and 10
+    secret_number = random.randrange(0, 10)
     guess_count = 0
-    guess_limit = 3
+    guess_limit = 3  # Set the maximum number of guesses allowed
 
+    # Start a loop for the player to make guesses
     while guess_count < guess_limit:
-        guess = int(input("Guess: "))
-        guess_count+=1
+        guess = int(input("Guess: "))  # Get the player's guess
+        guess_count += 1  # Increment the guess count
 
+        # Check if the guess is correct
         if guess == secret_number:
             print("Congrats. You Won!")
             try_again = input("Would you like to try again? 'Y' or 'N': ").lower()
 
+            # If the player wants to try again, call the repeat function
             if try_again == 'y':
-
                 repeat()
-                
+            # If the player chooses not to try again, provide an option to exit
             elif try_again == 'n':
                 to_exit = input("Enter 'E' to exit: ").lower()
                 
+                # If the player enters 'E', exit the program
                 if to_exit == 'e':
                     exit()
 
-        elif guess_count < guess_limit and guess!= secret_number:
+        # If the guess is incorrect and more attempts are available
+        elif guess_count < guess_limit and guess != secret_number:
             print("Sorry. Try again.")
-
+        
+        # If all attempts are used and the guess is still incorrect
         elif guess_count == guess_limit and guess != secret_number:
             print("Sorry, You Failed")
 
             try_again = input("Would you like to try again? 'Y' or 'N': ").lower()
 
+            # If the player wants to try again, call the repeat function
             if try_again == 'y':
                 repeat()
-
+            # If the player chooses not to try again, provide an option to exit
             elif try_again == 'n':
-
                 to_exit = input("Enter 'E' to exit: ").lower()
+                # If the player enters 'E', exit the program
                 if to_exit == 'e':
                     exit()
 
+# Call the repeat function to start the game
 repeat()
