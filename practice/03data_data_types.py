92f7c7136b7004931bc1a91bafcfb3c206200d1e
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-14 15:53:29 +0000

    Update 03data_data_types.py

--- practice/03data_data_types.py
+++ practice/03data_data_types.py
@@ -2,19 +2,43 @@
 
 # String -- str
 # String is a collection of characters declared within a single or double quotation mark
+name = 'Vaughn'
+fullName = 'Vaughn Adams'
+print(name)
+print(type(name)) # Prints the datatype of name
 
+print(fullName)
+print(type(fullName)) # Prints the datatype of fullName
 
 # Integer -- int
 # Integer is a positive or negative whole number
+age = 19
+Age = 25
+print(age)
+print(type(age))
 
+print(Age)
+print(type(Age))
 
 # Float -- float
 # A float is a fractional or decimal value
+height = 6.6
+myGPA = 3.95
 
+print(height)
+print(myGPA)
+print(type(myGPA))
 
 # Complex -- complex
 # A complex number is a number which consist of a real part and an imaginary part.
+myvalue = 3+5j
 
+print(myvalue)
+print(type(myvalue))
 
 # Boolean -- bool
 # A boolean data type represents data that can only exist in two forms.
+is_registered = True
+
+print(is_registered)
+print(type(is_registered))
