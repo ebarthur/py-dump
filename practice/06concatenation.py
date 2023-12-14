77e92da64aa058d01f0f1f2e9a2e63fcde12299c
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-14 16:36:01 +0000

    Update 06concatenation.py

--- practice/06concatenation.py
+++ practice/06concatenation.py
@@ -1,5 +1,11 @@
 # Python concatenation
 # This simply means joining two or more string values together
+firstName = "Genny"
+lastName = "McKinsen"
+fullName = firstName + lastName
 
+print(fullName)                   # GennyMcKinson
+print(firstName + " " + lastName) # Genny McKinsen
 
 # Formatted String
+print(f'My name is {firstName} {lastName}') # AMy name is Genny McKinsen
