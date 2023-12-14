61d2a9db77e883fb6cd417d27696a7aa14f6944d
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-14 14:28:37 +0000

    Update 01code.py

--- practice/01code.py
+++ practice/01code.py
@@ -1,18 +1,24 @@
 # My First Python Program
+print('Hello World') # This is a another comment
 # Comments
 # Comments are non-executable notes written to give some descriptive information on the kind of programming logic being implemented
 '''
-This can also be used as a comment; 
+This can also be used as a comment; This is a multi-line comment.
 '''
 """
-However, it has a more usefule purpose we will discuss later
+However, it has a more useful purpose we will discuss later
 """
 
-
 # print parameters
+print("Arthur", "Hughes", sep=" ") # sep is a separator.
 
+print("Jean", "Vaughn", end="\n") # end=('\n') puts every str value on a new line
+print("University of Utah")
 
 # Python language order of execution
+print('University of Utah') # First line of logic get executed and the second follows suit and so on.
+print('Loyola University')
+print('Carnegie Melon University')
 
 
 
