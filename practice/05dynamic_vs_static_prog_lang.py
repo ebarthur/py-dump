21e72ec0b54ae813f14e44d1e9b59ff9156bc353
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-14 16:46:30 +0000

    Update 05dynamic_vs_static_prog_lang.py

--- practice/05dynamic_vs_static_prog_lang.py
+++ practice/05dynamic_vs_static_prog_lang.py
@@ -1,4 +1,13 @@
 # Characteristics of Python programming language
+'''Python is a high-level, interpreted, and dynamically typed programming 
+language known for its readability and simplicity. It features a large standard 
+library, supports object-oriented programming, and has a vibrant community. 
+Python is versatile, used across various domains, and is platform-independent. 
+Its clean syntax, automatic memory management, and emphasis on code 
+readability make it an ideal choice for a wide range of applications, 
+from scripting to web development, data science, and machine learning.'''
+
+
 # Dynamic vs Static Programming Languages
 ''''Dynamic and static programming languages differ in how they handle variables and types during program execution.
 
@@ -24,12 +33,14 @@
 
 Ultimately, the choice depends on project requirements and the desired balance between flexibility, safety, and performance.'''
 
+
 # Interpreted vs Compiled languages
 '''Interpreted languages execute code line by line at runtime using an interpreter, allowing for platform independence and a faster development cycle. Examples include Python and JavaScript.
 
 Compiled languages translate code into machine or intermediate code before execution, resulting in platform-specific binaries. They often offer better performance but have a longer development cycle. Examples include C, C++, and Java (when compiled to bytecode).
 
 The choice between interpreted and compiled languages depends on factors such as performance, portability, development speed, and debugging preferences. Some languages, like Java, combine both approaches for a balance of performance and portability.'''
+
 
 # High level vs Low level languages
 '''High-level languages offer greater abstraction, readability, and portability,
