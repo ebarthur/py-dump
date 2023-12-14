b5c44634f46ff6c0aa57aab022b614c94e877374
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-14 16:08:00 +0000

    Update 04input_function.py

--- practice/04input_function.py
+++ practice/04input_function.py
@@ -1,1 +1,8 @@
 # Python input function
+from getpass import getpass # Hover on getpass to get the use of the module
+
+name = input("Enter your name: ") #Takes input from user
+password = getpass("Eenter your password: ") # getpass hides input to secure personal details.
+print(f'You entered {name}') #Prints out user's input. 
+print(password)
+
