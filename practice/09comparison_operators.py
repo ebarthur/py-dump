1de6ff7eef964a903692f786986e46bcb28a5f66
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-15 14:05:12 +0000

    Update 09comparison_operators.py

--- practice/09comparison_operators.py
+++ practice/09comparison_operators.py
@@ -1,16 +1,46 @@
 # Python comparison operators #  == < > <= >= !=
 # Comparison operators are used to compare two or more values
+x = 10
+y = 5
+z = 10
 
 # Equal to ==
+print(x==y) # False
+print(x==z) # True
 
 # Not Equal to !=
+print(x!=y) # True
+print(x!=z) # False
 
 # Greater than >
+print(x>y) # True
+print(x>z) # False
 
 # Less than <
+print(y<z) # True
+print(y<x) # True
 
 # Grater than or equal to >=
+print(y>=z) # False
+print(x>=z) # True
 
 # Less than or equal to <=
+print(y<=z) # True
+print(x<=z) # True
 
 # Assignment Operator: = += -= *=
+x = 7
+x == y
+
+x = 5
+x += 10   # Same as x = x + 10
+print(x)  # 15
+
+# Typical shopping cart
+total = 0
+item_price = 49
+total += item_price
+
+print(total) # 49
+
+# You can do same with -= and *=
