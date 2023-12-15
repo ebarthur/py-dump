352bf55b5e066d7e6d38f97b4967ffd0ca6b3e4d
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-15 22:52:23 +0000

    Update 11boolean_operators.py

--- practice/11boolean_operators.py
+++ practice/11boolean_operators.py
@@ -2,8 +2,27 @@
 # Boolean operators are used to combine conditional statements
 
 
-# and
+# and: Returns True when both conditions are true
+x = 10
+y = 5
+z = 10
 
-# or
+print(x == y and y < z) # False
+print(x == z and y < z) # True
+
+# or # Returns True when one of the two conditions is true
+a = 7
+b = 4
+c = 10
+
+print(a == b or b > a) # False 
+print(a <= b or b < c) # True 
 
 # not
+p = 10
+q = 12
+r = 15
+
+print(not(p==r)) # True
+
+
