84128d7f2b7f1a3599e6772013f8f00231b5e326
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-17 22:26:19 +0000

    Update 13python_tuple.py

--- practice/13python_tuple.py
+++ practice/13python_tuple.py
@@ -1,6 +1,23 @@
 # Python tuple is a collection which is ordered.
 # A tuple is not mutable
 # It allows duplicate members
+fruits= ('Mango', 'Apple', 'Banana')
+
+print(type(fruits)) # <class 'tuple'>
+print(fruits) # ('Mango', 'Apple', 'Banana')
+print(fruits[0]) # Mango
+
+# Count
+# Index
+print(fruits.count('Banana')) # 1
+print(fruits.index('Apple'))  # 1
+
+# Tuple does not allow assignment of items
+# It is not MUTABLE
 
 
 # Unpack Tuple
+x, *y = (2, 0, 9)
+
+print(y) # [0, 9] It unpacked into a list
+print(x) # 2
