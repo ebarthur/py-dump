217b169cb197dcc8f40ce3244028313a9c98f924
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-18 18:05:11 +0000

    Update 18python_class.py

--- practice/18python_class.py
+++ practice/18python_class.py
@@ -1,7 +1,22 @@
 # A class is a blue-print for creating objects.
+class bakingPan:
+    pass
 
+bread1 = bakingPan()
+# flour, sugar, special_ingredient
+bread1.flour = 'Soft'
+bread1.sugar = 20
+bread1.special_ingredient = 'wheat'
 
+print(bread1.flour) # Soft
+print(bread1.sugar) # 20
 
+#----------------------------
 
+bread2 = bakingPan()
+bread2.flour = 'Hard'
+bread2.sugar = 10
+bread2.special_ingredient = 'coconut'
 
-
+print(bread2.flour) # Hard
+print(bread2.sugar) # 10
