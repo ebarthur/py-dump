891411f079aaf8b43579cd167eb226ca850a241d
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-20 17:51:55 +0000

    Update class.py

--- practice/class.py
+++ practice/class.py
@@ -7,10 +7,14 @@
 # BakingPan.myFunction() # This is my baking pan function
 
 class BakingPan:
+    unit_price = 5
     def __init__(self, flour, sugar, special_ingredient):
         self.flour = flour
         self.sugar = sugar
         self.special_ingredient = special_ingredient
+        
+    def bread_name(self):
+            return f'{self.unit_price}'
         
 bread1 = BakingPan('Soft', 20, 'Wheat')
 bread2 = BakingPan('Hard', 10, 'Banana')
@@ -20,3 +24,4 @@ print(bread1.flour) # Soft
 print(bread1.special_ingredient) # Wheat
     
 print(bread2.special_ingredient) # Banana
+print(bread1.bread_name())
