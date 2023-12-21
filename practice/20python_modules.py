83829a3c15efb2baf0c867debd4bd7edbc75180c
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-21 18:07:27 +0000

    Update 20python_modules.py

--- practice/20python_modules.py
+++ practice/20python_modules.py
@@ -1,5 +1,10 @@
 # A python module is a piece of code/file that is imported and used in another python file
 # Inbuilt modules
+from keyword import kwlist
+from math import pi, prod # right click on a module >>> 'Go to definition' to get the code behind the module program
 
+print(pi) # 3.141592653589793
+print(kwlist) # returns all python keywords
+print(prod([1,2,3,4,5])) # 120  hover oon prod to get the definition
 
-# Custome modules
+# Custom modules
