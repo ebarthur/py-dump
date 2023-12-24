1340b7cc7ea39b53eb6ca5a53a6680ac6b02e9d2
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-24 21:40:27 +0000

    Update main.py

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -4,25 +4,26 @@ import random
 # print multiline instruction
 # perform string concatenation of string
 print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
-      + "Rock vs Paper -> Paper wins \n"
-      + "Rock vs Scissors -> Rock wins \n"
-      + "Paper vs Scissors -> Scissor wins \n")
+    + "Rock vs Paper -> Paper wins \n"
+    + "Rock vs Scissors -> Rock wins \n"
+    + "Paper vs Scissors -> Scissor wins \n")
 
 while True:
+
     print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
 
     # take the input from user
     choice = int(input("Enter your choice :"))
 
     # OR is the short-circuit operator
-    # if any one of the condition is true
+    # if any one of the conditions is true
     # then it returns True value
 
-    # looping until user enters invalid input
+    # looping until the user enters an invalid input
     while choice > 3 or choice < 1:
         choice = int(input('Enter a valid choice please '))
 
-    # initialize value of choice_name variable
+    # initialize the value of choice_name variable
     # corresponding to the choice value
     if choice == 1:
         choice_name = 'Rock'
@@ -33,11 +34,11 @@ while True:
 
     # print user choice
     print('User choice is \n', choice_name)
-    print('Now its Computers Turn....')
+    print('Now it\'s Computers Turn....')
 
     # Computer chooses randomly any number
     # among 1, 2, and 3. Using randint method
-    # of random module
+    # of the random module
     comp_choice = random.randint(1, 3)
 
     # looping until comp_choice value
@@ -45,7 +46,7 @@ while True:
     while comp_choice == choice:
         comp_choice = random.randint(1, 3)
 
-    # initialize value of comp_choice_name
+    # initialize the value of comp_choice_name
     # variable corresponding to the choice value
     if comp_choice == 1:
         comp_choice_name = 'rocK'
@@ -58,7 +59,7 @@ while True:
 
     # we need to check for a draw
     if choice == comp_choice:
-        print('Its a Draw', end="")
+        print('It\'s a Draw', end="")
         result = "DRAW"
     # condition for winning
     if (choice == 1 and comp_choice == 2):
@@ -84,14 +85,14 @@ while True:
 
     # Printing either user or computer wins or draw
     if result == 'DRAW':
-        print("<== Its a tie ==>")
+        print("<== It's a tie ==>")
     if result == choice_name:
         print("<== User wins ==>")
     else:
         print("<== Computer wins ==>")
     print("Do you want to play again? (Y/N)")
-    # if user input n or N then condition is True
-    ans = input().lower()
+    # if the user input n or N then the condition is True
+    ans = input().lower
     if ans == 'n':
         break
 
