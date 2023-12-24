36a866277d1dd8a515e8e46683cdb451211902fa
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-24 21:53:33 +0000

    File update

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -1,19 +1,23 @@
-# import random module
 import random
+import time
 
-# print multiline instruction
-# perform string concatenation of string
-print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
+def type_text(text, delay=0.05):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
+# print multiline instruction with typing effect
+type_text('Winning rules of the game ROCK PAPER SCISSORS are :\n'
     + "Rock vs Paper -> Paper wins \n"
     + "Rock vs Scissors -> Rock wins \n"
     + "Paper vs Scissors -> Scissor wins \n")
 
 while True:
+    type_text("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
 
-    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
-
-    # take the input from user
-    choice = int(input("Enter your choice :"))
+    # take the input from the user
+    choice = int(input("Enter your choice: "))
 
     # OR is the short-circuit operator
     # if any one of the conditions is true
@@ -33,8 +37,8 @@ while True:
         choice_name = 'Scissors'
 
     # print user choice
-    print('User choice is \n', choice_name)
-    print('Now it\'s Computers Turn....')
+    type_text('User choice is \n' + choice_name)
+    type_text('Now it\'s Computers Turn....')
 
     # Computer chooses randomly any number
     # among 1, 2, and 3. Using randint method
@@ -54,48 +58,37 @@ while True:
         comp_choice_name = 'papeR'
     else:
         comp_choice_name = 'scissoR'
-    print("Computer choice is \n", comp_choice_name)
-    print(choice_name, 'Vs', comp_choice_name)
+    type_text("Computer choice is \n" + comp_choice_name)
+    type_text(choice_name + ' Vs ' + comp_choice_name)
 
     # we need to check for a draw
     if choice == comp_choice:
-        print('It\'s a Draw', end="")
+        type_text('It\'s a Draw', end="")
         result = "DRAW"
     # condition for winning
-    if (choice == 1 and comp_choice == 2):
-        print('paper wins =>', end="")
-        result = 'papeR'
-    elif (choice == 2 and comp_choice == 1):
-        print('paper wins =>', end="")
+    elif (choice == 1 and comp_choice == 2) or (choice == 2 and comp_choice == 1):
+        type_text('paper wins =>', end="")
         result = 'Paper'
-
-    if (choice == 1 and comp_choice == 3):
-        print('Rock wins =>\n', end="")
+    elif (choice == 1 and comp_choice == 3) or (choice == 3 and comp_choice == 1):
+        type_text('Rock wins =>\n', end="")
         result = 'Rock'
-    elif (choice == 3 and comp_choice == 1):
-        print('Rock wins =>\n', end="")
-        result = 'rocK'
-
-    if (choice == 2 and comp_choice == 3):
-        print('Scissors wins =>', end="")
-        result = 'scissoR'
-    elif (choice == 3 and comp_choice == 2):
-        print('Scissors wins =>', end="")
-        result = 'Rock'
+    elif (choice == 2 and comp_choice == 3) or (choice == 3 and comp_choice == 2):
+        type_text('Scissors wins =>', end="")
+        result = 'Scissors'
 
     # Printing either user or computer wins or draw
     if result == 'DRAW':
-        print("<== It's a tie ==>")
-    if result == choice_name:
-        print("<== User wins ==>")
+        type_text("<== It's a tie ==>")
+    elif result == choice_name:
+        type_text("<== User wins ==>")
     else:
-        print("<== Computer wins ==>")
-    print("Do you want to play again? (Y/N)")
+        type_text("<== Computer wins ==>")
+    type_text("Do you want to play again? (Y/N)")
     # if the user input n or N then the condition is True
-    ans = input().lower
+    ans = input().lower()
     if ans == 'n':
         break
 
 # after coming out of the while loop
 # we print thanks for playing
-print("thanks for playing")
+type_text("Thanks for playing")
